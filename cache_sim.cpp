1985225c0c8a22c29a1da7b4a747e1f93189b56c
Julie Torres <jgt58@txstate.edu>
2025-11-06 19:30:41 -0600

    main

--- cache_sim.cpp
+++ cache_sim.cpp
@@ -5,3 +5,14 @@
 #include <cstdint>
 using namespace std;
 
+struct Line {
+    bool valid = false;
+    uint64_t tag = 0;
+};
+
+int main(int argc, char* argv[]){
+    if (argc!= 4){
+        cerr << "Usage: " << argv[0] << " <num_entries> <associativity> <input_file>\n";
+    return 1;
+    }
+}
