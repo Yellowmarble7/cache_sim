bf8556645c68029c57164dce712d7120d70f17d3
Julie Torres <jgt58@txstate.edu>
2025-11-07 00:35:32 -0600

    vector

--- cache_sim.cpp
+++ cache_sim.cpp
@@ -15,4 +15,17 @@ int main(int argc, char* argv[]){
         cerr << "Usage: " << argv[0] << " <num_entries> <associativity> <input_file>\n";
     return 1;
     }
-}
+
+ uint64_t num_sets = stoull(argv[1]);
+ uint64_t associativity = stoull(argv[2]);
+ string input_file = argv[3];
+
+
+if (num_sets == 0 || associativity == 0 || (num_sets % associativity) != 0) {
+        cerr << "num_sets must be > 0 and divisible by associativity.\n";
+        return 1;
+    }
+
+uint64_t num_sets = num_entries / associativity;
+
+vector<vector<Line>> cache(num_sets, vector<Line>(associativity));
